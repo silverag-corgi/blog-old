964749a80b37d1cb55138d9fff191078cfdbfb96
Rem_ <black.white.red.are.the.best@gmail.com>
2021-08-06 11:53:08 +0900

    [c_mod] Jekyllサーバ起動バッチ編集

--- _bat/02_ExecJekyllServer.bat
+++ _bat/02_ExecJekyllServer.bat
@@ -1,5 +1,8 @@
 @echo off
 
+echo Go Home Page
+start chrome.exe "http://localhost:4000/silverag-corgi.github.io/"
+
 echo Exec Jekyll Server
 cd ..
 bundle exec jekyll serve --baseurl "/silverag-corgi.github.io"
