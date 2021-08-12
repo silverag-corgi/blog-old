b0a2e4ec5baccbdedd86760dbd95e4e4809f669b
Rem_ <black.white.red.are.the.best@gmail.com>
2021-08-13 00:28:52 +0900

    [c_mod] テーマ(JekyllCleanDark)編集
    ページトップ移動ボタン設置

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,11 +1,39 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 50) {
-    $('nav').addClass('shrink');
-  } else {
-    $('nav').removeClass('shrink');
-  }
+// ナビ
+$(function() {
+	var eleNav = $('nav');
+	
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 50) {
+			eleNav.addClass('shrink');
+		} else {
+			eleNav.removeClass('shrink');
+		}
+	});
+
+	$(document).ready(function(){
+		$('[data-toggle="tooltip"]').tooltip();
+	});
 });
 
-$(document).ready(function(){
-  $('[data-toggle="tooltip"]').tooltip();
+// ページトップ
+$(function() {
+	var eleBtn = $('#page-top-card');
+	
+	// ボタン表示(スクロール50px以上)
+	eleBtn.hide();
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) {
+			eleBtn.fadeIn();
+		} else {
+			eleBtn.fadeOut();
+		}
+	});
+	
+	// ページトップへスクロール(1000ms)
+	eleBtn.click(function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1000);
+		return false;
+	});
 });
