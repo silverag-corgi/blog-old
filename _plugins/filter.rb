1e0445311972be95fb48345e9a7772c80ddb070f
Rem_ <black.white.red.are.the.best@gmail.com>
2021-09-11 00:39:15 +0900

    [c_mod] テーマ編集
    プラグイン作成
      - Filter.generate_page_navi
    画面で上記プラグインの処理に相当する箇所をプラグイン呼び出しに変更
      - 画面共通．ページナビ

--- _plugins/filter.rb
+++ _plugins/filter.rb
@@ -1,5 +1,6 @@
 module Jekyll
 	module Filter
+		require "uri"
 		
 		# タグ配列を人気のタグ配列に変換する(記事数：降順、タグ名：小文字昇順)
 		# 使用例：{% assign tags = site.tags | convert_into_popular_tags %}
@@ -42,6 +43,88 @@ module Jekyll
 			end
 			
 			return array_sorted_by_posts_count_with_order
+		end
+		
+		# URLからページナビ配列を生成する
+		# 使用例：{% assign page_navi_array = 
+		#             page.url | generate_page_navi: page.categories, site.data.words %}
+		#         {% assign page_name = page_navi_array[i][0] %}
+		#         {% assign page_path = page_navi_array[i][1] %}
+		def generate_page_navi(url, page_categories, data_words)
+			# 1.URLを"/"ごとに分割し、ページ配列に変換する
+			url_temp = url
+			url_temp = url_temp.gsub("index.html","")
+			url_temp = url_temp.gsub(".html","")
+			pages = url_temp.split("/")
+			
+			# 2.ページ配列のサイズが0の場合、ホームを表す空白文字を追加する
+			if pages.size == 0
+				pages.push("")
+			end
+			
+			# 3.ページ配列の要素ごとにページナビ配列を作成する
+			prev_path = ""
+			this_path = ""
+			is_category = false
+			cat_root_path = ""
+			page_navi_array = Array.new(0) { Array.new(2,"") }
+			pages.each_with_index do | page, index |
+				prev_path = this_path
+				this_path = URI.decode_www_form_component(this_path + page + "/")
+				page_eng = URI.decode_www_form_component(page)
+				page_navi_element = Array.new(2)
+				
+ 				if page == "page"        # ページがpageの場合
+					next
+				elsif page == "contents" # ページがcontentsの場合
+					next
+				else                     # ページが上記以外の場合
+					# 3-1.ページ名(英名)が単語帳に登録済みの場合、和名をページ名(和名)に設定する
+					page_jpn = ""
+					for word in data_words
+						if page_eng == word["eng"]
+							page_jpn = word["jpn"]
+							break
+						end
+					end
+					
+					# 3-2.ページ名(和名)が単語帳に
+					#     未登録の場合はページ名(英名)をページナビ要素に設定し、
+					#     登録済みの場合はページ名(和名)をページナビ要素に設定する
+					if page_jpn == ""
+						page_navi_element[0] = page_eng
+					else
+						page_navi_element[0] = page_jpn
+					end
+					
+					# 3-3.ページ名(英名)がカテゴリの場合、カテゴリフラグを真に設定し、
+					#     初回のみカテゴリルートパスを設定する
+					if page_categories != nil
+						for category in page_categories
+							if page_eng == category
+								is_category = true
+								if cat_root_path == ""
+									cat_root_path = prev_path
+								end
+							end
+						end
+					end
+					
+					# 3-4.カテゴリフラグが
+					#     偽の場合は現在のパスをページナビ要素に設定し、
+					#     真の場合は当該カテゴリのパスをページナビ要素に設定する
+					if is_category == false
+						page_navi_element[1] = this_path
+					else
+						page_navi_element[1] = URI.decode_www_form_component(cat_root_path + page + "/")
+					end
+					
+					# 3-3.ページナビ配列にページナビ要素を追加する
+					page_navi_array.push(page_navi_element)
+				end
+			end
+			
+			return page_navi_array
 		end
 		
 	end
